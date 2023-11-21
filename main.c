5d3a779afce6e378140e44dff0ad5581ea51f7d6
Densex4545 <147789317+Densetsu4545@users.noreply.github.com>
2023-11-21 11:13:49 +0900

    yyyy

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
 int main(){
-  printf("Hello World\n");
-  int a;
+  printf("Welcome to C Programming!\n");
+  int x = 10;
+  int y = x*2;
+  int z = x + y;
+  printf("x=%d,y=%d,z=%d\n",x,y,z);
+
+  int price = 500;
+  float price1 = price * 1.1;
+  printf("%.1f\n",price1);
 }
+
